b4ad67ba24c6f528a4e32a2038993c723ad0fa13
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2020-06-23 15:06:53 +0100

    Add files via upload

---
+++ examPrep/config.js
@@ -0,0 +1,38 @@
+let courseConfig = {
+    'CIT213' : {
+        '2019/2' : ['fbq', 'objectives'],
+    },
+
+    'CIT215' : {
+        '2019/2' : ['fbq'],
+    },
+
+    'CIT237' : {
+        '2019/2' : ['fbq', 'objectives'],
+    },
+
+    'CIT211' : {
+        '2019/2' : ['fbq', 'objectives'],
+    },
+    
+    'ESM211' : {
+        '2019/2' : ['fbq'],
+    },
+
+    'BIO201' : {
+        '2019/2' : ['fbq', 'objectives'],
+    },
+    
+    'BIO203' : {
+        '2019/2' : ['fbq', 'objectives'],
+    },
+    
+    'BIO213' : {
+        '2019/2' : ['fbq'],
+    },
+
+    'BIO217' : {
+        '2019/2' : ['fbq', 'objectives'],
+    },
+
+}
