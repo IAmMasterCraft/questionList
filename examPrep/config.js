a5da154204013512547685f59b89ae0b9c0dac95
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-02-06 02:20:20 +0100

    Update config.js

--- examPrep/config.js
+++ examPrep/config.js
@@ -1,219 +1,226 @@
 let courseConfig = {
-    'ESM211' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'ACC102' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'ACC201' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'ACC203' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'ACC204' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'ACC210' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'AEM202' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'AEM203' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'ANP204' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'ARA282' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'ARD251' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BED212' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BED214' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BFN203' : {
-        '2019/2' : ['fbq'],
+    'ESM211': {
+        '2019/2': ['fbq'],
     },
 
-    'BIO102' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BIO191' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BIO192' : {
-        '2019/2' : ['fbq'],
+    'ACC102': {
+        '2019/2': ['fbq'],
     },
 
-    'BIO201' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'BIO203' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'BIO206' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BIO210' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BIO213' : {
-        '2019/2' : ['fbq'],
+    'ACC201': {
+        '2019/2': ['fbq', 'objectives'],
     },
 
-    'BIO217' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'BIO218' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BIO220' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BUS106' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'BUS207' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CHM101' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CHM192' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CIT104' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CIT202' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CIT211' : {
-        '2019/2' : ['fbq', 'objectives'],
-        'QUESTION BANKS' : ['question_bank_1', 'question_bank_2', 'question_bank_3', 'question_bank_4', 'question_bank_5', 'question_bank_6', 'question_bank_7', 'question_bank_8'],
-    },
-    
-    'CIT213' : {
-        '2019/2' : ['fbq', 'objectives'],
-        'QUESTION BANKS' : ['question_bank_1'],
+    'ACC203': {
+        '2019/2': ['fbq'],
     },
 
-    'CIT215' : {
-        '2019/2' : ['fbq'],
+    'ACC204': {
+        '2019/2': ['fbq'],
     },
 
-    'CIT237' : {
-        '2019/2' : ['fbq', 'objectives'],
-    },
-    
-    'CIT246' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CIT292' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CRD120' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CRD206' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CRS111' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CRS215' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'CRS231' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'MTH213' : {
-        '2019/2' : ['fbq'],
-    },
-    
-    'MTH232' : {
-        '2019/2' : ['fbq'],
+    'ACC210': {
+        '2019/2': ['fbq'],
     },
 
-    'CIT102' : {
-        '2019/2' : ['fbq', 'objectives'],
+    'AEM202': {
+        '2019/2': ['fbq', 'objectives'],
     },
 
-    'ECO121' : {
-        '2019/2' : ['fbq'],
+    'AEM203': {
+        '2019/2': ['fbq', 'objectives'],
     },
 
-    'CSS133' : {
-        '2019/2' : ['fbq'],
+    'ANP204': {
+        '2019/2': ['fbq', 'objectives'],
     },
 
-    'CSS111' : {
-        '2019/2' : ['fbq', 'objectives'],
+    'ARA282': {
+        '2019/2': ['fbq'],
     },
 
-    'GST201' : {
-        '2019/2' : ['fbq', 'objectives'],
-        'QUESTION BANKS' : ['question_bank_1', 'question_bank_2', 'question_bank_3', 'question_bank_4', 'question_bank_5', 'question_bank_6', 'question_bank_7', 'question_bank_8'],
+    'ARD251': {
+        '2019/2': ['fbq'],
     },
 
-    'GST103' : {
-        '2019/2' : ['fbq', 'objectives'],
+    'BED212': {
+        '2019/2': ['fbq'],
     },
 
-    'PCR111' : {
-        '2019/2' : ['fbq'],
+    'BED214': {
+        '2019/2': ['fbq'],
     },
 
-    'PCR115' : {
-        '2019/2' : ['fbq'],
+    'BFN203': {
+        '2019/2': ['fbq'],
     },
 
-    'GST203' : {
-        '2019/2' : ['fbq', 'objectives'],
-        'QUESTION BANKS' : ['question_bank_1', 'question_bank_2', 'question_bank_3', 'question_bank_4', 'question_bank_5', 'question_bank_6', 'question_bank_7', 'question_bank_8', 'question_bank_9', 'question_bank_10'],
+    'BIO102': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO191': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO192': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO201': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'BIO203': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'BIO206': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO210': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO213': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO217': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'BIO218': {
+        '2019/2': ['fbq'],
+    },
+
+    'BIO220': {
+        '2019/2': ['fbq'],
+    },
+
+    'BUS106': {
+        '2019/2': ['fbq'],
+    },
+
+    'BUS207': {
+        '2019/2': ['fbq'],
+    },
+
+    'CHM101': {
+        '2019/2': ['fbq'],
+    },
+
+    'CHM192': {
+        '2019/2': ['fbq'],
+    },
+
+    'CIT104': {
+        '2019/2': ['fbq'],
+    },
+
+    'CIT202': {
+        '2019/2': ['fbq'],
+    },
+
+    'CIT211': {
+        '2019/2': ['fbq', 'objectives'],
+        'QUESTION BANKS': ['question_bank_1', 'question_bank_2', 'question_bank_3', 'question_bank_4', 'question_bank_5', 'question_bank_6', 'question_bank_7', 'question_bank_8'],
+    },
+
+    'CIT213': {
+        '2019/2': ['fbq', 'objectives'],
+        'QUESTION BANKS': ['question_bank_1'],
+    },
+
+    'CIT215': {
+        '2019/2': ['fbq'],
+    },
+
+    'CIT237': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'CIT246': {
+        '2019/2': ['fbq'],
+    },
+
+    'CIT292': {
+        '2019/2': ['fbq'],
+    },
+
+    'CRD120': {
+        '2019/2': ['fbq'],
+    },
+
+    'CRD206': {
+        '2019/2': ['fbq'],
+    },
+
+    'CRS111': {
+        '2019/2': ['fbq'],
+    },
+
+    'CRS215': {
+        '2019/2': ['fbq'],
+    },
+
+    'CRS231': {
+        '2019/2': ['fbq'],
+    },
+
+    'MTH213': {
+        '2019/2': ['fbq'],
+    },
+
+    'MTH232': {
+        '2019/2': ['fbq'],
+    },
+
+    'CIT102': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'ECO121': {
+        '2019/2': ['fbq'],
+    },
+
+    'CSS133': {
+        '2019/2': ['fbq'],
+    },
+
+    'CSS111': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'GST201': {
+        '2019/2': ['fbq', 'objectives'],
+        'QUESTION BANKS': ['question_bank_1', 'question_bank_2', 'question_bank_3', 'question_bank_4', 'question_bank_5', 'question_bank_6', 'question_bank_7', 'question_bank_8'],
+    },
+
+    'GST103': {
+        '2019/2': ['fbq', 'objectives'],
+    },
+
+    'PCR111': {
+        '2019/2': ['fbq'],
+    },
+
+    'PCR115': {
+        '2019/2': ['fbq'],
+    },
+
+    'GST203': {
+        '2019/2': ['fbq', 'objectives'],
+        'QUESTION BANKS': ['question_bank_1', 'question_bank_2', 'question_bank_3', 'question_bank_4', 'question_bank_5', 'question_bank_6', 'question_bank_7', 'question_bank_8', 'question_bank_9', 'question_bank_10'],
+    },
+
+    'GST202': {
+        '2019/2': ['fbq'],
+    },
+
+    'GST204': {
+        '2019/2': ['fbq'],
     },
 
 }
-
