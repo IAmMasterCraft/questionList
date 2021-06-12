9d2212d442f0a7e80f4a4139e76f25dc56e56091
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-06-12 19:50:33 +0100

    Update setup_auto.js

--- setup_auto.js
+++ setup_auto.js
@@ -2,6 +2,7 @@ const allCourses = {
     "Ym9sdTgwMjE=": true,
     "Q0hPOTc2OQ==": true,
     "QUdTMTkyNg==": true,
+    "U0FNMTk0Ng==": true,
     /*"U01PMTAwNA==": true,
     "QVNUMTAxNQ==": true,
     "QUVMMTIwMg==": true,
